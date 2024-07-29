96fd37f5d15888cb92d7589faa776c5b775a494d
thinknathan <github@thinknathan.com>
2024-07-28 19:29:23 -0700

    outline of ludobits

---
+++ packages/ludobits/ludobits.m.app.d.ts
@@ -0,0 +1,10 @@
+/** @noSelfInFile */
+
+/**
+ * Utilities for game development using the Defold engine 
+ * @see {@link https://github.com/britzl/ludobits|Github Source}
+ * @noResolution
+ */
+declare module 'ludobits.m.app' {
+	
+}
---
+++ packages/ludobits/ludobits.m.json.d.ts
@@ -0,0 +1,17 @@
+/** @noSelfInFile */
+
+/**
+ * @deprecated rxi.json has been removed and replaced by Defold native json.encode and json.decode
+ * @noResolution
+ */
+declare module 'ludobits.m.json' {
+	/**
+	 * @deprecated rxi.json has been removed and replaced by Defold native json.encode and json.decode
+	 */
+	export const decode: typeof json.decode;
+
+	/**
+	 * @deprecated rxi.json has been removed and replaced by Defold native json.encode and json.decode
+	 */
+	export const encode: typeof json.encode;
+}
